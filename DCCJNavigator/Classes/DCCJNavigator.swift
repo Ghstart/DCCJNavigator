7968987fa19b3556d251e023ca2b68e8ea474751
龚欢 <gonghuan2020@gmail.com>
2018-07-26 16:35:14 +0800

    first commit

---
+++ DCCJNavigator/Classes/DCCJNavigator.swift
@@ -0,0 +1,32 @@
+//
+//  DCCJNavigator.swift
+//  DCCJNavigator
+//
+//  Created by 龚欢 on 2018/7/26.
+//
+
+import Foundation
+
+public protocol DCCJNavigator {
+    associatedtype Destination
+    
+    func navigator(to destination: Destination)
+    func present(to destination: Destination, from vc: UIViewController)
+    func open(url: String)
+}
+
+extension DCCJNavigator {
+    func present(to destination: Destination, from vc: UIViewController) {}
+    
+    func open(url: String) {
+        guard let u = URL(string: url) else {
+            return
+        }
+        
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(u, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(u)
+        }
+    }
+}
---
+++ Example/Pods/Target Support Files/DCCJNavigator/DCCJNavigator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DCCJNavigator : NSObject
+@end
+@implementation PodsDummy_DCCJNavigator
+@end
